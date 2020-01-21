0da7fdcb25ff9847655f316c99867069d892d726
James Gary <gmarchesi888@gmail.com>
2020-01-21 17:05:22 -0500

    Express server now talking to DB.

--- src/components/Dashboard.jsx
+++ src/components/Dashboard.jsx
@@ -4,6 +4,22 @@ import Portfolio from "./Portfolio.jsx";
 import Navbar from "./Navbar.jsx";
 
 class Dashboard extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      watchListEntries: [
+        {
+          symbol: "XYZ",
+          name: "Xtreme Yoga Zone",
+          currentPrice: 99.87
+        },
+        
+      ],
+      portfolioEntries: [
+        { name: "AAPL", numOfShares: 59, buyingPrice: 300, dateBought: 200116 }
+      ]
+    };
+  }
   render() {
     return (
       <div>
