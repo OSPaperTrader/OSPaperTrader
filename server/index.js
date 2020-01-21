a4b6d2b9f00bcac618ed30800e08732b7eead4a9
James Gary <gmarchesi888@gmail.com>
2020-01-21 12:15:17 -0500

    Endpoint '/watchListData' returns Ethereum quote in JSON format.

--- server/index.js
+++ server/index.js
@@ -1,28 +1,27 @@
-const express = require('express');
-const path = require('path');
-const mainController = require('./controller.js');
+const express = require("express");
+const path = require("path");
+const mainController = require("./controller.js");
 
 const PORT = process.env.PORT || 3000;
-const bodyParser = require('body-parser');
-
+const bodyParser = require("body-parser");
 
 const app = express();
-// const stock = require('../routes/stock');
 
-const DIST_DIR = path.join(__dirname, '../dist');
-const HTML_FILE = path.join(DIST_DIR, 'index.html');
-
+const DIST_DIR = path.join(__dirname, "../dist");
+const HTML_FILE = path.join(DIST_DIR, "index.html");
 
 app.use(bodyParser.json());
 app.use(express.static(DIST_DIR));
 
-// app.use('/api/stock', stock);
-
-app.get('/api', mainController.getBTCData, (req, res) => {
+app.get("/api", mainController.getBTCData, (req, res) => {
   res.send(res.locals.data);
 });
 
-app.get('/', (req, res) => {
+app.get("/watchListData", mainController.getWatchListData, (req, res) => {
+  res.sendFile(res.locals.data);
+});
+
+app.get("/", (req, res) => {
   res.status(200).sendFile(HTML_FILE);
 });
 
