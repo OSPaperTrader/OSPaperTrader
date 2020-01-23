c90a76119efa24134cd1ba02be040026788205d5
James Gary <gmarchesi888@gmail.com>
2020-01-23 15:25:18 -0500

    Transaction Table working.

--- src/components/TransactionRow.jsx
+++ src/components/TransactionRow.jsx
@@ -1,20 +1,41 @@
-import React from 'react';
+import React from "react";
 
 class TransactionRow extends React.Component {
   render() {
-    console.log('Transactionrow:  ', this.props.stockData);
+    console.log("Transaction row:  ", this.props.stockData);
     return (
-      <div>
-        <p className="stockData">
+      <tr>
+        <td>
           {new Date(
             Date.parse(this.props.stockData.trans_date)
           ).toLocaleDateString()}
-        </p>
-        <p className="stockData">{this.props.stockData.symbol}</p>
-        <p className="stockData">{this.props.stockData.purchased_price}</p>
-        <p className="stockData">{this.props.stockData.qty_purchased}</p>
-        <p className="stockData">{this.props.currPrice}</p>
-      </div>
+        </td>
+        <td>{this.props.stockData.symbol}</td>
+        <td>{this.props.stockData.purchased_price}</td>
+        <td>{this.props.stockData.qty_purchased}</td>
+        <td>{this.props.currPrice || "unavailable"}</td>
+        {/* <th> </th>
+        <td className="stockData">
+          {new Date(
+            Date.parse(this.props.stockData.trans_date)
+          ).toLocaleDateString()}
+        </td>
+        <td className="stockData">{this.props.stockData.symbol}</td>
+        <td className="stockData">{this.props.stockData.purchased_price}</td>
+        <td className="stockData">{this.props.stockData.qty_purchased}</td>
+        <td className="stockData">{this.props.currPrice || "unavailable"}</td>
+        <td>
+          <button
+            onClick={() =>
+              console.log(
+                "Sell Button Clicked for " + this.props.stockData.symbol
+              )
+            }
+          >
+            Sell
+          </button>
+        </td> */}
+      </tr>
     );
   }
 }
