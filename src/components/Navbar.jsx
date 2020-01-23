7ab76905be2145aea70e2bb5ef62257754aca094
James Gary <gmarchesi888@gmail.com>
2020-01-23 13:47:57 -0500

    Minor styling tweaks.

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,7 +1,6 @@
 import React from "react";
 import { Link } from "react-router-dom";
 
-
 class Navbar extends React.Component {
   constructor(props) {
     super();
@@ -9,21 +8,19 @@ class Navbar extends React.Component {
 
   render() {
     return (
-          <div className="topnav">
-            <div className="nav-logo">
-              <h3>PaperTrader®</h3>
-            </div>
-            <Link to="/signup">
-              <button>Register</button>
-            </Link>
-            <Link to='/login'>
-            <button>Login</button>
-            </Link>
-          </div>
+      <div className="topnav">
+        <div className="nav-logo">
+          <h2>PaperTrader®</h2>
+        </div>
+        <Link to="/signup">
+          <button>Register</button>
+        </Link>
+        <Link to="/login">
+          <button>Login</button>
+        </Link>
+      </div>
     );
   }
 }
-
-
 
 export default Navbar;
